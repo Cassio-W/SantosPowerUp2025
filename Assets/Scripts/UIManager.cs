5cf7dbb4ea484c43dd5a5d78a935119086776b63
PeterBala <pedro.augusto200224@gmail.com>
2026-09-04 19:35:41 -0300

    Alterações nos atributos
    inverti os atributos das respostas. a esquerda concordando, a direita discordando.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -152,6 +152,7 @@ public class UIManager : MonoBehaviour
             LeanTween.move(dealPanel.GetComponent<RectTransform>(), new Vector3(0, -900, 0), 1f).setEase(easeType).setOnComplete(DeactivatePanel);
             LeanTween.move(datePanel.GetComponent<RectTransform>(), new Vector3(710, 651, 0), 1f).setEase(easeType);
             LeanTween.move(corruptionPanel.GetComponent<RectTransform>(), new Vector3(-120, -120, 0), 1f).setEase(easeType);
+            GameManager.instance.ChooseLeft(GameManager.instance.actualDeck[0]);
             StartCoroutine(GameManager.instance.ApplyDecision(GameManager.instance.actualDeck[0], GameManager.instance.actualDeck[0].impactsLeft));
             GameManager.instance.PPUnfocus();
         }
@@ -172,6 +173,7 @@ public class UIManager : MonoBehaviour
             LeanTween.move(dealPanel.GetComponent<RectTransform>(), new Vector3(0, -900, 0), 1f).setEase(easeType).setOnComplete(DeactivatePanel);
             LeanTween.move(datePanel.GetComponent<RectTransform>(), new Vector3(710, 651, 0), 1f).setEase(easeType);
             LeanTween.move(corruptionPanel.GetComponent<RectTransform>(), new Vector3(-120, -120, 0), 1f).setEase(easeType);
+            GameManager.instance.ChooseRight(GameManager.instance.actualDeck[0]);
             StartCoroutine(GameManager.instance.ApplyDecision(GameManager.instance.actualDeck[0], GameManager.instance.actualDeck[0].impactsRight));
             GameManager.instance.PPUnfocus();
         }
@@ -195,7 +197,7 @@ public class UIManager : MonoBehaviour
         {
             if (perks[i] == null) continue;
 
-            if (i < GameManager.instance.activePerks.Length && GameManager.instance.activePerks[i] != null)
+            if (i < GameManager.instance.activePerks.Count && GameManager.instance.activePerks[i] != null)
             {
                 perks[i].gameObject.SetActive(true);
                 perks[i].sprite = GameManager.instance.activePerks[i].icon;
